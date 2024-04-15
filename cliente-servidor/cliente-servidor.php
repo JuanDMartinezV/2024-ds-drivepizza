7cac14a05396425ad8996b6fd4c487540a986353
Juan Diego Martinez Vasquez <juandiegomv2303@gmail.com>
2024-04-15 17:31:40 -0500

    Entrega 15 abril

---
+++ cliente-servidor/cliente-servidor.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Drive pizza</title>
+    </head>
+    <body>
+    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $tamaño = $_POST['tamaño'];
+    $ingredientesSeleccionados = $_POST['ingredientes'];
+
+    
+    $precioBase = 10000; 
+    $precioPorIngrediente = 5000; 
+
+    if ($tamaño === "MED") {
+        $precioBase += 10000;
+    } elseif ($tamaño === "GRA") {
+        $precioBase += 20000;
+    }
+
+
+    $cantidadIngredientes = count($ingredientesSeleccionados);
+    if ($cantidadIngredientes === 1) {
+        $precioTotal = $precioBase;
+    } elseif ($cantidadIngredientes >= 2 && $cantidadIngredientes <= 4) {
+        $precioTotal = $precioBase + 5000;
+    } else {
+        $precioTotal = $precioBase + 7000;
+    }
+
+
+    echo "<h2>Resumen de la Reservación:</h2>";
+    echo "<p><strong>Fecha:</strong> $fecha</p>";
+    echo "<p><strong>Hora:</strong> $hora</p>";
+    echo "<p><strong>Tamaño de la Pizza:</strong> $tamaño</p>";
+    echo "<p><strong>Ingredientes:</strong> ";
+    foreach ($ingredientesSeleccionados as $ingrediente) {
+        echo "$ingrediente, ";
+    }
+    echo "</p>";
+    echo "<p><strong>Precio Total:</strong> $precioTotal colones</p>";
+}
+?>
+
+
+
+    </body>
+</html>
